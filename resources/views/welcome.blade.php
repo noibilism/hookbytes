7e628d2e23a2cd30f3c8fd0d18312c83f8fb1e9d
noibilism <noibilism@gmail.com>
2025-09-10 02:21:51 +0100

    Fix font loading issue with bunny.net CDN

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,8 @@
     <title>HookBytes - Never miss an event.</title>
     
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:300,400,500,600,700,800&display=swap" rel="stylesheet" />
     
     <!-- Styles -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
